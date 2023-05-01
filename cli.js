57500e489e059cce0901e6de8d60297759663c2e
gigimerry <69761091+gigimerry@users.noreply.github.com>
2023-05-01 17:29:45 -0400

    Update cli.js

--- cli.js
+++ cli.js
@@ -31,6 +31,7 @@ else {
 
 // latitude
 let latitude;
+
 if (args.n && args.s) {
     console.log("Please only enter ONE latitude! North OR South!")
 }
@@ -46,6 +47,9 @@ if (args.n) {
 // longitude
 let longitude;
 
+if (args.e && args.w) {
+    console.log("Please only enter ONE longitude! East OR West!")
+}
 if (args.e) {
 	longitude = args.e;
 } else if (args.w) {
@@ -74,12 +78,14 @@ if (args.d) {
 	days = 1;
 }
 
+rain = data.daily.precipitation_hours
+
 if (days == 0) {
-	console.log(data.daily.precipitation_hours[0] + " " + "today.");
+	console.log(rain[0] + " " + "today.");
 } else if (days == 1) {
-	console.log(data.daily.precipitation_hours[1] + " " + "tomorrow.");
+	console.log(rain[1] + " " + "tomorrow.");
 } else if (days > 1) {
-	console.log (data.daily.precipitation_hours[days] + " " + "in " + days + " days.");
+	console.log (rain[days] + " " + "in " + days + " days.");
 }
 
 
@@ -89,90 +95,3 @@ if (args.j) {
 }
 
 
-// import fetch from "node-fetch";
-// import moment from "moment-timezone";
-// import minimist from "minimist";
-
-// const args = minimist(process.argv.slice(2));
-
-
-// if (args.h) {
-// 	console.log(`Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
-//     -h            Show this help message and exit.
-//     -n, -s        Latitude: N positive; S negative.
-//     -e, -w        Longitude: E positive; W negative.
-//     -z            Time zone: uses tz.guess() from moment-timezone by default.
-//     -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.
-//     -j            Echo pretty JSON from open-meteo API and exit.`);
-// 	process.exit(0);
-// }
-//  let timezone;
-//  let latitude;
-//  let longitude;
-
-//  if (args.z) {
-//     timezone = args.z;
-//  }
-//  else {
-//     timezone = moment.tz.guess();
-//  }
-
-
-
-// if (args.e && args.w) {
-//     console.log("Please only enter ONE longitude! East OR West!")
-// }
-
-// if (args.e) {
-//     longitude = args.e;
-// } else if (args.w) {
-//     longitude = -args.w;
-// } else {
-//     console.log("Longitude must be in range!")
-// }
-
-
-
-// if (args.n && args.s) {
-//     console.log("Please only enter ONE latitude! North OR South!")
-// }
-
-// if (args.n) {
-//     latitude = args.n;
-// } else if (args.s) {
-//     latitude = -args.n;
-// } else {
-//     console.log("Latitude must be in range!")
-// }
-
-
-// const response = await fetch("https://api.open-meteo.com/v1/forecast?latitude=" 
-//                                 + latitude + "&longitude=" 
-//                                 + longitude + "&timezone=" 
-//                                 + timezone + "&daily=precipitation_hours");
-// const data = await response.json();
-// console.log(data);
-// let days;
-
-// if (args.d) {	
-// 	days = args.d;
-// } else if (args.d == 0) {
-// 	days = 0;
-// } else {
-// 	days = 1;
-// }
-
-
-// if (days == 0) {
-// 	console.log(data.daily.precipitation_hours[0] + " " + "today.");
-// } else if (days == 1) {
-// 	console.log(data.daily.precipitation_hours[1] + " " + "tomorrow.");
-// } else if (days > 1) {
-// 	console.log (data.daily.precipitation_hours[days] + " " + "in " + days + " days.");
-// }
-
-
-// if (args.j) {
-// 	console.log(data);
-// 	process.exit(0);
-// }
