13d804b4cacd28011ed5164f1a464dfede8fea57
gigimerry <69761091+gigimerry@users.noreply.github.com>
2023-05-01 17:12:58 -0400

    Update cli.js

--- cli.js
+++ cli.js
@@ -63,16 +63,8 @@ const response = await fetch("https://api.open-meteo.com/v1/forecast?latitude="
                                 + timezone + "&daily=precipitation_hours");
 const data = await response.json();
 console.log(data);
-const days = args.d;
-const rain = data.daily.precipitation_hours;
 
-if (days == 0) {
-    console.log("today.")
-} else if (days > 1) {
-    console.log("in" + days + "days.")
-} else {
-    console.log("tomorrow.")
-}
+
 
 
 
